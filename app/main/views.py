eaf52ce2a3996de7db232628aa693d6b83502247
pvpnvz <pvpnvz@users.noreply.github.com>
2016-09-13 20:13:45 +0800

    .

--- app/main/views.py
+++ app/main/views.py
@@ -2492,6 +2492,7 @@ def summary_init(internId):
     os.system('mkdir %s/%s/attachment' % (STORAGE_FOLDER, internId))
     os.system('mkdir %s/%s/summary_doc' % (STORAGE_FOLDER, internId))
     os.system('mkdir %s/%s/score_img' % (STORAGE_FOLDER, internId))
+    os.system('mkdir %s/%s/agreement' % (STORAGE_FOLDER, internId))
     os.system('mkdir %s/%s/score_img/comscore' % (STORAGE_FOLDER, internId))
     os.system('mkdir %s/%s/score_img/schscore' % (STORAGE_FOLDER, internId))
     return 1
