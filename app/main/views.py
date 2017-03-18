f550f0e06f6353224ac2ca9fef1635497cc21646
waveyan <593310640@qq.com>
2017-03-18 06:46:39 -0700

    登陆时教师完善教师信息

--- app/main/views.py
+++ app/main/views.py
@@ -2266,7 +2266,6 @@ def editTeacher():
     teaId = request.args.get('teaId')
     tea = Teacher.query.filter_by(teaId=teaId).first()
     #普通教师基本信息修改
-    m=request.args.get('m')
     if request.method == 'POST':
         try:
             tea.teaId = form.teaId.data
@@ -2278,7 +2277,7 @@ def editTeacher():
             db.session.add(tea)
             db.session.commit()
             flash('修改成功！')
-            if current_user.can(Permission.TEA_INFOR_MANAGE)and m is None:
+            if current_user.can(Permission.TEA_INFOR_MANAGE):
                 return redirect(url_for('.teaUserList'))
             else:
                 return redirect(url_for('.index'))
@@ -4829,4 +4828,4 @@ def getIntroduce_json():
 def improveTeaInfor():
     if not (current_user.teaEmail and current_user.teaPhone):
         flash('请先完善相关信息！')
-    return redirect(url_for('.editTeacher',teaId=current_user.get_id(),m='m'))
+    return redirect(url_for('.editTeacher',teaId=current_user.get_id()))
